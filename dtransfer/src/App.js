b8ccf7cf00df5a223ed7f06edbd23c06d4864d6d
jenks-guo-linktree <jenksguo@gmail.com>
2021-12-07 23:58:31 +1100

    uploaded logo created new styling, using react-bootstrap now

--- dtransfer/src/App.js
+++ dtransfer/src/App.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { FileUploader } from './components/FileUploader';
 import { Previewer } from './components/Previewer';
+import { Container, Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from './assets/dwetransfer-logo.png';
+import Background from './assets/background.jpg';
 
 function App() {
   const [files, setFiles] = useState([]);
@@ -10,9 +14,21 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <FileUploader onSuccess={onSuccess}/>
-      <Previewer files={files}/>
+    <div className="App" style={{ backgroundImage: `url(${Background})` }}>
+      <Container className="p-3">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Logo} />
+            <Card.Body>
+              <Card.Text>
+                It's like WeTansfer but via IPFS in the Web 3.0...  
+              </Card.Text>
+              <FileUploader onSuccess={onSuccess}/>
+              <Previewer files={files}/>
+            </Card.Body>
+          </Card>
+      </Container>
+    
+
     </div>
   );
 }
